c75d0337d4109360f157c279c4aa483b158f6ed1
Zakhar <zax_2000@mail.ru>
2025-03-03 17:37:36 +0500

    Оптимизация Matrix.cs
    Хронология изменений:
    1. Двумерный массив пикселей изменён на одномерный.
    2. Выбрано более строгое ограничение на тип данных.
    3. Методы приведения типов переписаны с использованием указателей.
    4. Добавлены константные значения из класса Pixel.cs.
    5. Добавлен расчёт значений полей пикселя при приведении Bitmap в Matrix.cs.

--- optimizations/JPEG/Images/Matrix.cs
+++ optimizations/JPEG/Images/Matrix.cs
@@ -1,65 +1,122 @@
 ﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+using JPEG.Processor;
 
 namespace JPEG.Images;
 
-class Matrix
+public class Matrix(int height, int width)
 {
-	public readonly Pixel[,] Pixels;
-	public readonly int Height;
-	public readonly int Width;
+    public readonly Pixel[] Pixels = new Pixel[height * width];
+    public readonly int Height = height;
+    public readonly int Width = width;
 
-	public Matrix(int height, int width)
-	{
-		Height = height;
-		Width = width;
+    private const float Coef1 = 16.0f;
+    private const float Coef2 = 65.738f;
+    private const float Coef3 = 129.057f;
+    private const float Coef4 = 24.064f;
+    private const float Coef5 = 256.0f;
+    private const float Coef6 = 128.0f;
+    private const float Coef7 = -37.945f;
+    private const float Coef8 = 74.494f;
+    private const float Coef9 = 112.439f;
+    private const float Coef10 = 94.154f;
+    private const float Coef11 = 18.285f;
 
-		Pixels = new Pixel[height, width];
-		for (var i = 0; i < height; ++i)
-		for (var j = 0; j < width; ++j)
-			Pixels[i, j] = new Pixel(0, 0, 0, PixelFormat.RGB);
-	}
+    public static unsafe explicit operator Matrix(Bitmap bmp)
+    {
+        var height = bmp.Height - bmp.Height % JpegProcessor.DctSize;
+        var width = bmp.Width - bmp.Width % JpegProcessor.DctSize;
+        var result = new Matrix(height, width);
 
-	public static explicit operator Matrix(Bitmap bmp)
-	{
-		var height = bmp.Height - bmp.Height % 8;
-		var width = bmp.Width - bmp.Width % 8;
-		var matrix = new Matrix(height, width);
+        var lockedBitmapData = bmp
+            .LockBits(
+                new Rectangle(
+                    0,
+                    0,
+                    width,
+                    height),
+                ImageLockMode.ReadOnly,
+                bmp.PixelFormat);
 
-		for (var j = 0; j < height; j++)
-		{
-			for (var i = 0; i < width; i++)
-			{
-				var pixel = bmp.GetPixel(i, j);
-				matrix.Pixels[j, i] = new Pixel(pixel.R, pixel.G, pixel.B, PixelFormat.RGB);
-			}
-		}
+        byte* scan0 = (byte*)lockedBitmapData.Scan0;
+        var i = 0;
+        var index = 0;
+        byte red;
+        byte green;
+        byte blue;
+        for (var j = 0; j < height; j++)
+        {
+            byte* row = scan0 + j * lockedBitmapData.Stride;
+            for (i = 0; i < width; i++)
+            {
+                index = i * JpegProcessor.ChannelCount;
+                red = row[index + 2];
+                green = row[index + 1];
+                blue = row[index];
+                result.Pixels[j * width + i] = new Pixel(
+                    Coef1 + (Coef2 * red + Coef3 * green + Coef4 * blue) / Coef5,
+                    Coef6 + (Coef7 * red - Coef8 * green + Coef9 * blue) / Coef5,
+                    Coef6 + (Coef9 * red - Coef10 * green - Coef11 * blue) / Coef5);
+            }
+        }
 
-		return matrix;
-	}
+        bmp.UnlockBits(lockedBitmapData);
+        return result;
+    }
 
-	public static explicit operator Bitmap(Matrix matrix)
-	{
-		var bmp = new Bitmap(matrix.Width, matrix.Height);
+    public static unsafe explicit operator Bitmap(Matrix matrix)
+    {
+        var height = matrix.Height;
+        var width = matrix.Width;
+        var pixelFormat = PixelFormat.Format24bppRgb;
+        var result = new Bitmap(width, height, pixelFormat);
+        var lockedBitmapData = result
+            .LockBits(
+                new Rectangle(
+                    0,
+                    0,
+                    width,
+                    height),
+                ImageLockMode.WriteOnly,
+                pixelFormat);
+        var scan0 = (byte*)lockedBitmapData.Scan0;
+        var j = 0;
+        fixed (Pixel* ptr = matrix.Pixels)
+        {
+            var pixel = ptr;
+            for (var i = 0; i < height; i++, scan0 += lockedBitmapData.Stride)
+            {
+                var row = scan0;
+                for (j = 0; j < width; j++, pixel += 1)
+                {
+                    *row = ToByte(pixel->Value3);
+                    row += 1;
+                    *row = ToByte(pixel->Value2);
+                    row += 1;
+                    *row = ToByte(pixel->Value1);
+                    row += 1;
+                }
+            }
+        }
 
-		for (var j = 0; j < bmp.Height; j++)
-		{
-			for (var i = 0; i < bmp.Width; i++)
-			{
-				var pixel = matrix.Pixels[j, i];
-				bmp.SetPixel(i, j, Color.FromArgb(ToByte(pixel.R), ToByte(pixel.G), ToByte(pixel.B)));
-			}
-		}
+        result.UnlockBits(lockedBitmapData);
+        return result;
+    }
 
-		return bmp;
-	}
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static byte ToByte(float d)
+    {
+        if (d > byte.MaxValue)
+        {
+            return byte.MaxValue;
+        }
 
-	public static int ToByte(double d)
-	{
-		var val = (int)d;
-		if (val > byte.MaxValue)
-			return byte.MaxValue;
-		if (val < byte.MinValue)
-			return byte.MinValue;
-		return val;
-	}
+        if (d < byte.MinValue)
+        {
+            return byte.MinValue;
+        }
+
+        return (byte)d;
+    }
 }
